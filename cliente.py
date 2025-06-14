89df85179dcf79715662f2074dc9a03f06f0838b
Jia_Ming_Liou <dliou471@gmail.com>
2025-06-14 09:38:03 -0600

    cliente

--- cliente.py
+++ cliente.py
@@ -1,10 +1,21 @@
 import utils as u
 
 def leer_clientes():
+    """
+        Esta funcion lee los clientes 
+        Returns:
+            lista: Lista de clientes en forma de diccionario
+    """      
     return u.leer_archivo(u.NOMBRE_ARCHIVO_CLIENTES)
 
 
 def agregar_cliente():
+    """
+        Esta funcion crear el archivo de cliente si no existe
+            Si existe archivo lee archivo de clientes
+            Recibe numero de cedula y valida si ya existe un usuario con esa cedula
+            En caso de no existir recibe los valores faltantes del cliente y escribe sobre el archivo de cliente
+    """        
     if u.existe_archivo(u.NOMBRE_ARCHIVO_CLIENTES) == False:
         u.crear_archivo(u.NOMBRE_ARCHIVO_CLIENTES, [])
 
@@ -35,6 +46,13 @@ def agregar_cliente():
 
 
 def editar_cliente():
+    """
+        Esta funcion edita nombre, telefono, dirrecion y correo del cliente
+            Si el archivo de clientes no existe se detiene el proceso
+            Lee el archivo del cliente
+            Filtra las cedulas existentes 
+            Recibe los valores nuevos y despues los guarda en el archivo de clientes nuevamente
+    """      
     cedula = u.ingresar_valor("Ingrese una cedula", False)
     if u.existe_archivo(u.NOMBRE_ARCHIVO_CLIENTES) == False:
          print("No se ha encontrado el archivo de clientes")
@@ -62,3 +80,21 @@ def editar_cliente():
     otros_clientes.append(cliente_editado)
     u.crear_archivo(u.NOMBRE_ARCHIVO_CLIENTES, otros_clientes)
     print("Cliente editado con exito")
+
+def eliminar_cliente():
+    """
+        Esta funcion borra el cliente con la cedula aportada
+    """          
+    cedula = u.ingresar_valor("Ingrese una cedula", False)
+    if u.existe_archivo(u.NOMBRE_ARCHIVO_CLIENTES) == False:
+         print("No se ha encontrado el archivo de clientes")
+         
+    clientes = u.leer_archivo_cliente(u.NOMBRE_ARCHIVO_CLIENTES)
+    cliente_encontrado = [cliente for cliente in clientes if cliente["cedula"] == cedula] 
+    if len(cliente_encontrado) == 0:
+        print(f"Cliente con {cedula} no encontrado")
+        return
+    
+    otros_clientes = [cliente for cliente in clientes if cliente["cedula"] != cedula]
+    u.crear_archivo(u.NOMBRE_ARCHIVO_CLIENTES, otros_clientes)
+    print(f"Cliente con {cedula}, removido con exito")
--- usuario.py
+++ usuario.py
@@ -2,6 +2,15 @@ NOMBRE_DE_USUARIO = "python"
 PASS = "python123"
 
 def validar_usuario(usuario, contrasena):
+    """
+        Esta funcion valida el usuario y contasena digitado por el administrador 
+         Args:
+            usuario (str): Usuario del administrador
+            contrasena (str): Contrasena del administrador
+
+        Returns:
+            bool: Si el usuario esta autenticado
+    """  
     if usuario != NOMBRE_DE_USUARIO or contrasena != PASS:
         return False
     
