1341be2cc1e1e22046ab4a1accee72b9be2700c4
jesusvillalobosmurillo <503470157@castrocarazo.ac.cr>
2025-07-09 18:32:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,52 +12,56 @@ import usuario as us
 
 # Definición de la función principal
 if __name__ == "__main__":
+    
+    while True:
+        usuario = input("Introduzca el usuario: ")
+        password = input("Introduzca su contrasena: ")
 
-    usuario = input("Introduzca el usuario: ")
-    password = input("Introduzca su contrasena: ")
+        usuario_validado = us.validar_usuario(usuario, password)
 
-    usuario_validado = us.validar_usuario(usuario, password)
-
-    if usuario_validado:
-        # Elige entre interfaz gráfica o menú de consola
-        modo = input("Selecciona modo (1: Interfaz gráfica, 2: Consola): ")
-        if modo == "1":
-            # interfase.interfaz()
-            pass
-        elif modo == "2":
-            menu_principal = True
-            while menu_principal:
-                # Mostrar el menú
-                opcion = menu()
-                
-                # Ejecutar la opción seleccionada
-                if opcion == "1":
-                    crear_orden_trabajo()
-                elif opcion == "2":
-                    consultar_orden_trabajo()
-                elif opcion == "3":
-                    modificar_orden_trabajo()
-                elif opcion == "4":
-                    eliminar_orden_trabajo()
-                elif opcion == "5":
-                    generar_reporte_ordenes_dia()
-                elif opcion == "6":
-                    generar_reporte_ingresos_mes()
-                elif opcion == "7":
-                    generar_reporte_uso_impresoras()
-                elif opcion == "8":
-                    cl.agregar_cliente()
-                elif opcion == "9":
-                    cl.editar_cliente()                                
-                elif opcion == "10":
-                    print("Saliendo del programa...")
-                    menu_principal = False
-                else:
-                    print("Opción no válida. Intente de nuevo.")
+        if usuario_validado:
+            # Elige entre interfaz gráfica o menú de consola
+            modo = input("Selecciona modo (1: Interfaz gráfica, 2: Consola): ")
+            if modo == "1":
+                # interfase.interfaz()
+                pass
+                break
+            elif modo == "2":
+                menu_principal = True
+                while menu_principal:
+                    # Mostrar el menú
+                    opcion = menu()
+                    
+                    # Ejecutar la opción seleccionada
+                    if opcion == "1":
+                        crear_orden_trabajo()
+                    elif opcion == "2":
+                        consultar_orden_trabajo()
+                    elif opcion == "3":
+                        modificar_orden_trabajo()
+                    elif opcion == "4":
+                        eliminar_orden_trabajo()
+                    elif opcion == "5":
+                        generar_reporte_ordenes_dia()
+                    elif opcion == "6":
+                        generar_reporte_ingresos_mes()
+                    elif opcion == "7":
+                        generar_reporte_uso_impresoras()
+                    elif opcion == "8":
+                        cl.agregar_cliente()
+                    elif opcion == "9":
+                        cl.editar_cliente()                                
+                    elif opcion == "10":
+                        print("Saliendo del programa...")
+                        menu_principal = False
+                    else:
+                        print("Opción no válida. Intente de nuevo.")
+                break
+            else:
+                print("Modo no válido. Saliendo del programa.")
+                break
         else:
-            print("Modo no válido. Saliendo del programa.")
-    else:
-        print("Usuario o contasena incorrecto")        
+            print("Usuario o contasena incorrecto")        
             
 
 
